b698c196e44ac43b7b0f976c1ea4bf12ddd9a60e
Val <valexx55@gmail.com>
2022-02-01 11:02:14 +0100

    chuck

--- chucknorris/chucknorris.js
+++ chucknorris/chucknorris.js
@@ -1,4 +1,4 @@
-fetch("https://api.chucknorris.io/jokes/random")
+/*fetch("https://api.chucknorris.io/jokes/random")
         .then(response => response.json())//deserializa : pasa de texto JSON a Variable JS
         .then((frasechuck) => {
             console.log("frase RECIBIDA " +frasechuck.value);
@@ -7,4 +7,18 @@ fetch("https://api.chucknorris.io/jokes/random")
         })
         .catch(function (error) {
             console.error('Hubo un problema con la petición Fetch:' + error.message);
-        });
+        });*/
+const URL_SERVICIO_CHUCK = "https://api.chucknorris.io/jokes/random";
+function obtenerFrase()
+{
+    console.log("Obtener frase");
+    fetch(URL_SERVICIO_CHUCK)
+        .then(respuesta => respuesta.json())
+        .then(info_frase => {
+            //extraer la frase y la imagen
+            document.getElementById("frasechuck").innerHTML=info_frase.value;
+            document.getElementById("imagenchuck").src = info_frase.icon_url;
+            //y dibujarlas en el html
+        })
+
+}

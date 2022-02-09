60004957225c20124b7ef3ff95b8d7cda4ef2d8c
Val <valexx55@gmail.com>
2022-02-09 08:55:57 +0100

    add clase Marcador

--- angular/my-app/src/app/components/marcador/marcador.component.ts
+++ angular/my-app/src/app/components/marcador/marcador.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Marcador } from 'src/app/marcador';
 
 @Component({
   selector: 'app-marcador',
@@ -7,19 +8,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MarcadorComponent implements OnInit {
 
-  marcador_jugador:number;
+  /* marcador_jugador:number;
   marcador_maquina:number;
+   */
+
+  marcador:Marcador;
+
   constructor() { 
-    this.marcador_jugador=0;
-    this.marcador_maquina=0;
+    /* this.marcador_jugador=0;
+    this.marcador_maquina=0; */
+    this.marcador = new Marcador();
 
     console.log("constructor MarcadorComponent");
     
   }
   ponerMarcadorAcero()
 {
-  this.marcador_jugador=0;
-  this.marcador_maquina=0;
+  /* this.marcador_jugador=0;
+  this.marcador_maquina=0; */
+  this.marcador = new Marcador();
 }
 
   ngOnInit(): void {
@@ -31,7 +38,7 @@ export class MarcadorComponent implements OnInit {
 
   leerMarcadorGuardado ()
   {
-    let marcador_aux_j = localStorage.getItem('puntuacion_jugador');
+    /* let marcador_aux_j = localStorage.getItem('puntuacion_jugador');
     let marcador_aux_m = localStorage.getItem('puntuacion_maquina');
  
     if (marcador_aux_j)
@@ -42,6 +49,12 @@ export class MarcadorComponent implements OnInit {
     if (marcador_aux_m)
     {
       this.marcador_maquina =+marcador_aux_m;
+    } */
+
+    let string_json_resultado = localStorage.getItem('puntuacion_jugador');
+    if (string_json_resultado)
+    {
+      this.marcador = <Marcador>JSON.parse(string_json_resultado);
     }
 
   }
@@ -52,15 +65,15 @@ export class MarcadorComponent implements OnInit {
   if (resultado==1)
   {
     //ganao el jugador
-    this.marcador_jugador= this.marcador_jugador+1;
+    this.marcador.marcador_jugador= this.marcador.marcador_jugador+1;
   } else if (resultado==-1)
   {
     //ha ganao el pc
-    this.marcador_maquina=this.marcador_maquina+1;
+    this.marcador.marcador_maquina=this.marcador.marcador_maquina+1;
   }else {
     //son empate
-    this.marcador_jugador= this.marcador_jugador+1;
-    this.marcador_maquina=this.marcador_maquina+1;
+    this.marcador.marcador_jugador= this.marcador.marcador_jugador+1;
+    this.marcador.marcador_maquina=this.marcador.marcador_maquina+1;
   }
 
 
@@ -68,8 +81,13 @@ export class MarcadorComponent implements OnInit {
 
 guardarMarcador ()
 {
-  localStorage.setItem('puntuacion_jugador',this.marcador_jugador.toString());
-  localStorage.setItem('puntuacion_maquina',this.marcador_maquina.toString());
+  /* localStorage.setItem('puntuacion_jugador',this.marcador_jugador.toString());
+  localStorage.setItem('puntuacion_maquina',this.marcador_maquina.toString()); */
+  
+  let string_json_resultado = JSON.stringify(this.marcador);
+  //localStorage.setItem('puntuacion_jugador',this.marcador_jugador.toString());
+  localStorage.setItem('puntuacion_jugador',string_json_resultado);
+
 }
 
 ngOnDestroy() {
---
+++ angular/my-app/src/app/marcador.ts
@@ -0,0 +1,14 @@
+export class Marcador {
+
+    marcador_jugador:number;
+    marcador_maquina:number;
+  
+
+
+    constructor()
+    {
+        this.marcador_maquina=0;
+        this.marcador_jugador=0;
+
+    }
+}

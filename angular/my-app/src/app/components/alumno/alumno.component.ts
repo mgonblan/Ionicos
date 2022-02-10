9c6140a502e7d15c69b5addfce5cdd88f6ef90f0
Val <valexx55@gmail.com>
2022-02-10 13:08:40 +0100

    toca borrar

--- angular/my-app/src/app/components/alumno/alumno.component.ts
+++ angular/my-app/src/app/components/alumno/alumno.component.ts
@@ -1,3 +1,4 @@
+import { HttpEventType, HttpHeaders, HttpResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { Alumno } from 'src/app/models/alumno';
 import { AlumnoService } from 'src/app/services/alumno.service';
@@ -23,9 +24,19 @@ export class AlumnoComponent implements OnInit {
     console.error('Ha ocurrido un error: (' + error.status + ') - ' + error.message);
   }
 
+  mostrarCabeceras (http_response:HttpResponse<Array<Alumno>>)
+  {
+    console.log(`ESTATUS ${http_response.status} ${ http_response.statusText}`);
+    console.log(`TIPO ${http_response.headers.get('content-type')}`);
+    
+  }
+
+  deleteUsuario(alumno:Alumno){
+    console.log("borrar usuario " + alumno.id);
+  }
+
   ngOnInit(): void {
     //aquí es el sitio para obtener datos de fuera
-    //ngOnInit
     this.servicio_alumnos.obtenerAlumnos().subscribe(
       //"observador"--el objeto que reciba la llamada 
       //cuando la respuesta esté lista
@@ -36,6 +47,22 @@ export class AlumnoComponent implements OnInit {
         next: (listado_alumnos_rx) =>
         {
           this.lista_alumnos = listado_alumnos_rx;
+          this.lista_alumnos.forEach (alumno => {console.log(`Alumno ${alumno.id} ${alumno.nombre} `)});
+        }
+      }
+    );
+
+    this.servicio_alumnos.obtenerAlumnosConCabeceras().subscribe(
+      //"observador"--el objeto que reciba la llamada 
+      //cuando la respuesta esté lista
+      {
+        complete: () => {console.log("ha terminado");},
+        error: (error_r) => {this.mostrarError(error_r);},
+        //error: (error_r) => {console.error('FALLLO ' +error_r);},
+        next: (http_rx) =>
+        {
+          this.mostrarCabeceras(http_rx);
+          this.lista_alumnos = <Array<Alumno>>http_rx.body;//CASTING Body genérico a un Array de Alumnos
           this.lista_alumnos.forEach (alumno => {console.log(`Alumno ${alumno.id} ${alumno.nombre} `)});
         }
       }
--- angular/my-app/src/app/services/alumno.service.ts
+++ angular/my-app/src/app/services/alumno.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Alumno } from '../models/alumno';
@@ -25,6 +25,17 @@ export class AlumnoService {
   {
    return this.http.get<Array<Alumno>>("http://10.1.2.10:8085/"); 
    //return this.http.get<Alumno[]>("http://10.1.2.10:8090/api/alumnos"); 
+  }
+
+  obtenerAlumnosConCabeceras():Observable<HttpResponse<Array<Alumno>>>
+  {
+   return this.http.get<Array<Alumno>>("http://10.1.2.10:8085/", {observe: 'response'}); 
+   //return this.http.get<Alumno[]>("http://10.1.2.10:8090/api/alumnos"); 
+  }
+
+  borrarAlumno (id:number):Observable<void>
+  {
+    return this.http.delete<void>("http://10.1.2.10:8085/"+id);
   }
 
 }

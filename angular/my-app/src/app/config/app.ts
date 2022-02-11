74b6b87df5444360f19a9a6ee2ddb05463a86977
Val <valexx55@gmail.com>
2022-02-11 10:05:32 +0100

    json server

---
+++ angular/my-app/src/app/config/app.ts
@@ -0,0 +1,2 @@
+export const RUTA_SERVIDOR_CLASE = "http://10.1.2.10:3000/alumno/"
+export const RUTA_JSON_SERVER = "http://localhost:3000/alumno/"
--- angular/my-app/src/app/services/alumno.service.ts
+++ angular/my-app/src/app/services/alumno.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient, HttpResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { RUTA_JSON_SERVER, RUTA_SERVIDOR_CLASE } from '../config/app';
 import { Alumno } from '../models/alumno';
 
 /*esto es un servicio
@@ -11,6 +12,10 @@ con el servidor*/
   providedIn: 'root'
 })
 export class AlumnoService {
+
+
+  //ruta_servidor:string=RUTA_SERVIDOR_CLASE;
+  ruta_servidor:string=RUTA_JSON_SERVER;
 
   constructor(private http:HttpClient) {
 
@@ -23,19 +28,19 @@ export class AlumnoService {
   //borrarAlumno
   obtenerAlumnos():Observable<Array<Alumno>>
   {
-   return this.http.get<Array<Alumno>>("http://10.1.2.10:8085/"); 
+   return this.http.get<Array<Alumno>>(this.ruta_servidor); 
    //return this.http.get<Alumno[]>("http://10.1.2.10:8090/api/alumnos"); 
   }
 
   obtenerAlumnosConCabeceras():Observable<HttpResponse<Array<Alumno>>>
   {
-   return this.http.get<Array<Alumno>>("http://10.1.2.10:8085/", {observe: 'response'}); 
+   return this.http.get<Array<Alumno>>(this.ruta_servidor, {observe: 'response'}); 
    //return this.http.get<Alumno[]>("http://10.1.2.10:8090/api/alumnos"); 
   }
 
   borrarAlumno (id:number):Observable<void>
   {
-    return this.http.delete<void>("http://10.1.2.10:8085/"+id);
+    return this.http.delete<void>(this.ruta_servidor+id);
   }
 
 }
